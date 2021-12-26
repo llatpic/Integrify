77fc9f3a20b41351cce80360d5d62f42710bf4af
llatpic <81221183+llatpic@users.noreply.github.com>
2021-12-26 19:30:09 +0100

    Revert "First attempt"
    This reverts commit b26969b298113f4133b479476a7bba0ac2c47997.

--- 00 Pre-Assignment/Section B/main.js
+++
@@ -1,7 +0,0 @@
-
-// Get the button that opens the modal
-var canvi = document.getElementById("task-new");
-
-// When the user clicks on <span> (x), close the modal
-canvi.onclick = function() {
-  div.style.backround-color = "blue";

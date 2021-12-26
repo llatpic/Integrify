b26969b298113f4133b479476a7bba0ac2c47997
llatpic <81221183+llatpic@users.noreply.github.com>
2021-12-26 19:08:27 +0100

    First attempt

---
+++ 00 Pre-Assignment/Section B/main.js
@@ -0,0 +1,7 @@
+
+// Get the button that opens the modal
+var canvi = document.getElementById("task-new");
+
+// When the user clicks on <span> (x), close the modal
+canvi.onclick = function() {
+  div.style.backround-color = "blue";

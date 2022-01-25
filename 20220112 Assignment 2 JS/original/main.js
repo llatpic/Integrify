e097b43a91f303362982ba813d89a28215b84f6e
llatpic <81221183+llatpic@users.noreply.github.com>
2022-01-25 13:15:57 +0100

    Assignment 2

---
+++ 20220112 Assignment 2 JS/original/main.js
@@ -0,0 +1,53 @@
+// variables
+
+// 1. Declare 3 variables with the name of a, b and c. a has type of string, b has type of number, and c has type of number
+// a and b can not be re-assigned, c can be re-assigned
+// the variable value can be of your choice
+
+// 2. Write an if statement that has the following logic:
+// if the length of a is larger than 5, print "a has more than 5 characters" to console, else print "a has less than 5 characters"
+// if b is larger than 0 and smaller than 5, print "ping", else print "pong"
+// if c is larger than b, print "c is larger than b", else print "c is smaller than b"
+
+// functions
+
+// 1. Write a function that will take 2 numbers as inputs, then return the sum of the 2 numbers.
+function add() {}
+
+// 2. Create a similar function as the above, but return the multiple of the 2 numbers.
+function multiply() {}
+
+// 3. Write a function that accepts a number as input, if the number is odd, return a string 'odd', if this number is even, return a string 'even'.
+function oddOrEven() {}
+
+// 5. Fix this function. We want to see 2 in the console instead of undefined
+function hoisting() {
+  console.log(y) // undefined
+  let y = 2
+}
+
+// 6. Write a count function that runs from 1 to 100 using for loop
+// if the current count is odd, print "odd", else print "even"
+function count() {}
+
+// 7. Write a function that accepts a string as an input, and return a new string with the first character uppercased
+// Ex: capitalized("abc") will return "Abc"
+function capitalized() {}
+
+// 8. Write a function accepts a number as input and then return the number reversed
+// Ex: reverse(1234) will return 4321
+function reverse() {}
+
+// 9. Write a function accepts a string as input and checks if that string is a palindrome. A palindrome is word, phrase, or sequence that reads the same backward as forward, Ex: madam
+// if the string is a palindrome, return true, else return false
+function isPalindrome() {}
+
+// 10. Write a JavaScript function that accepts two numbers and print the larger
+// Ex: printLarger(1,2) will console log 2 in the console
+function printLarger() {}
+
+// 11. Write a JavaScript function to extract a specified number of characters from a string
+// Ex: extract("abcd", 2) will return "ab"
+// Ex: extract("abcd", 3) will return "abc"
+
+// 10. Transform all of the above into arrow functions below here
